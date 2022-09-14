d6cdb7ba1dc7c4147137aa0ec9a2119e5b354f47
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2022-09-14 22:43:28 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created By: Spencer Scarlett
+# Date: 9/14/2022
+# This program displays Hello, World! with proper style.
+
+
+def main():
+    print("Hello, World! with proper style!")
+
+
+if __name__ == "__main__":
+    main()
